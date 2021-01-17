b968ae85445f40fd1de9a372597309211c6ec9c0
Baz Lever <bazlever6@gmail.com>
2021-01-17 23:06:08 +1100

    I hope this doesn't break anything!

---
+++ Assets/Scripts/DeliveryNode.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryNode : MonoBehaviour
+{
+
+    public int nodeNumber;
+    public bool isActive;
+
+    public GameObject TriggerMesh;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (isActive == true)
+        {
+            TriggerMesh.SetActive(true);
+            gameObject.GetComponent<CapsuleCollider>().enabled = true;
+        } else if (isActive == false)
+        {
+            TriggerMesh.SetActive(false);
+            gameObject.GetComponent<CapsuleCollider>().enabled = false;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            NodeTriggered();
+        }
+    }
+
+    public void NodeTriggered()
+    {
+        isActive = false;
+        GameObject.Find("NodeManager").GetComponent<NodeManager>().NewNode();
+    }
+}
---
+++ Assets/Scripts/NodeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeManager : MonoBehaviour
+{
+
+    public GameObject[] nodes;
+
+    void Start()
+    {
+        NewNode();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void NewNode()
+    {
+        int i = Random.Range(1, nodes.Length);
+
+        nodes[i].gameObject.GetComponent<DeliveryNode>().isActive = true;
+    }
+}
