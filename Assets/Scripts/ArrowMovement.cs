8e25618ad85dd77f91b729e55e96644832c98b03
Riley McCarthy <rileybenjaminmcc@hotmail.com>
2021-01-20 17:23:59 +1100

    Arrow set up but not following player
    Arrow direction working but not pointing at current node yet

---
+++ Assets/Scripts/ArrowMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMovement : MonoBehaviour
+{
+    public GameObject pointAt;
+    private GameObject player;
+    
+    //public GameObject player;
+    //Vector3 currentPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.FindGameObjectWithTag("Player");
+        pointAt.GetComponent<Transform>();
+        //transform.position = new Vector3 (player.transform.position.x,player.transform.position.y + arrowHeight,player.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = player.transform.localPosition;
+        transform.LookAt(pointAt.transform);
+        //this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, pointAt.transform.rotation, 1* Time.deltaTime);
+        /*        currentPos = new Vector3(player.transform.position.x, player.transform.position.y + 2.5f, player.transform.position.z);
+                transform.position = currentPos;*/
+    }
+}
