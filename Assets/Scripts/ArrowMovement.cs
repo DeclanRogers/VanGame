85aec16dfab9debbb89168136737c17fe42191ba
Riley McCarthy <rileybenjaminmcc@hotmail.com>
2021-01-26 12:15:50 +1100

    GPS fix. ArrowMovement script spring cleaning

--- Assets/Scripts/ArrowMovement.cs
+++ Assets/Scripts/ArrowMovement.cs
@@ -4,26 +4,17 @@ using UnityEngine;
 
 public class ArrowMovement : MonoBehaviour
 {
-    public GameObject pointAt;
-    private GameObject player;
     public GameObject nodeManager;
-    public NodeManager node;
+    private NodeManager node;
   
     void Start()
     {
         node = nodeManager.GetComponent<NodeManager>();
-        //player = GameObject.FindGameObjectWithTag("Player");
-        pointAt.GetComponent<Transform>();
-        //transform.position = new Vector3 (player.transform.position.x,player.transform.position.y + arrowHeight,player.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position = player.transform.localPosition;
         transform.LookAt(node.currentNode.transform);
-        //this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, pointAt.transform.rotation, 1* Time.deltaTime);
-        /*        currentPos = new Vector3(player.transform.position.x, player.transform.position.y + 2.5f, player.transform.position.z);
-                transform.position = currentPos;*/
     }
 }
